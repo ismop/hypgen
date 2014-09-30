37a0254be674e82b5c7e569697e74a77f50663ab
Marek Kasztelnik <mkasztelnik@gmail.com>
2014-09-30 12:06:07 +0200

    options without http basic auth

--- lib/hypgen/api/v1/main.rb
+++ lib/hypgen/api/v1/main.rb
@@ -7,9 +7,14 @@ module Hypgen
         version 'v1', using: :header, vendor: :dice
         format :json
 
-        http_basic do |username, password|
-          Hypgen.config.username == username &&
-            Hypgen.config.password == password
+        before do
+          unless env['REQUEST_METHOD'] == 'OPTIONS'
+            auth = Rack::Auth::Basic::Request.new(env)
+            valid = Hypgen.config.username == auth.username &&
+                      Hypgen.config.password == auth.credentials.last
+
+            throw(:error, status: 401, message: 'API Authorization Failed.') unless valid
+          end
         end
 
         mount Hypgen::Api::V1::Experiment => '/api'
