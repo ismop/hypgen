fc9d39e41a2c48f3f492cb83f9515403d5be4f5d
Marek Kasztelnik <mkasztelnik@gmail.com>
2015-01-09 07:36:32 +0100

    use dap url from configuration in rest response
    DAP url was hardcoded in rest API response.

--- lib/hypgen/api/v1/experiment.rb
+++ lib/hypgen/api/v1/experiment.rb
@@ -28,7 +28,7 @@ module Hypgen
             status 201
             {
               meta: {
-                url: "https://dap.moc.ismop.edu.pl/api/v1/experiments/#{exp.id}"
+                url: "#{Hypgen.config.dap_url}/api/v1/experiments/#{exp.id}"
               },
               experiment: {
                 id: exp.id
