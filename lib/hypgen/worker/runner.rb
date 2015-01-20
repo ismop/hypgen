7bfcfb8026cf6cfb4193e19b047b7917e7788423
Maciej Pawlik <m.pawlik@cyfronet.pl>
2015-01-20 12:41:05 +0100

    Fix long line to satisfy hound.

--- lib/hypgen/worker/runner.rb
+++ lib/hypgen/worker/runner.rb
@@ -26,7 +26,8 @@ module Hypgen
           if return_code == 0
             Hypgen.dap.update_exp(id, status: :finished)
           else
-            Hypgen.dap.update_exp(id, status: :error, status_message: "Non zero return code!")
+            Hypgen.dap.update_exp(id, status: :error,
+                                  status_message: "Non zero return code!")
           end
         end
       rescue StandardError => e
