edbd13e816e19b6ae7b3bbcb888c16392095b715
Marek Kasztelnik <mkasztelnik@gmail.com>
2015-02-19 07:43:39 +0100

    profile_id renamed into section_id in api

--- lib/hypgen/api/v1/threat_assessments.rb
+++ lib/hypgen/api/v1/threat_assessments.rb
@@ -11,7 +11,7 @@ module Hypgen
           params do
             requires :threat_assessment, type: Hash  do
               requires :name, type: String
-              requires :profile_ids, type: Array
+              requires :section_ids, type: Array
               requires :start_date, type: Time
               requires :end_date, type: Time
               optional :deadline, type: Integer, default: 500
@@ -20,7 +20,7 @@ module Hypgen
           post do
             exp = Hypgen::Experiment.new(
               params[:threat_assessment][:name],
-              params[:threat_assessment][:profile_ids],
+              params[:threat_assessment][:section_ids],
               params[:threat_assessment][:start_date],
               params[:threat_assessment][:end_date],
               params[:threat_assessment][:deadline])
