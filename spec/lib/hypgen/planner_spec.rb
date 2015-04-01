cbe47e7d2ebe3bea77f8be8b33faa26578a25324
Maciej Malawski <malawski@agh.edu.pl>
2015-04-01 13:20:06 +0200

    Reading number of days from the workflow object.

--- spec/lib/hypgen/planner_spec.rb
+++ spec/lib/hypgen/planner_spec.rb
@@ -13,6 +13,8 @@ describe Hypgen::Planner do
       [
         { init_conf_tmp_id: 7, params: { experimentId: @experimentId, dap_token: Hypgen.config.dap_token } },
       ]}
+    allow(workflow).to receive(:start_time) {"2014-11-19T09:45:42.025Z"}
+    allow(workflow).to receive(:end_time) {"2014-11-20T09:45:42.025Z"}
     planner = Hypgen::Planner.new(workflow)
     setup     = planner.setup
 
@@ -22,10 +24,37 @@ describe Hypgen::Planner do
 
   end
 
-  it 'should compute performance model' do
+
+  it 'should read start and end time from workflow' do
 
     workflow = double("workflow")
     allow(workflow).to receive(:deadline) {60}
+
+    allow(workflow).to receive(:start_time) {"2014-11-19T09:45:42.025Z"}
+    allow(workflow).to receive(:end_time) {"2014-11-20T09:45:42.025Z"}
+    planner = Hypgen::Planner.new(workflow)
+    expect(planner.days).to eq(1)
+
+    allow(workflow).to receive(:start_time) {"2014-11-19T09:45:42.025Z"}
+    allow(workflow).to receive(:end_time) {"2014-11-21T09:45:42.025Z"}
+    planner = Hypgen::Planner.new(workflow)
+    expect(planner.days).to eq(2)
+
+    allow(workflow).to receive(:start_time) {"2014-11-19T09:45:42.035Z"}
+    allow(workflow).to receive(:end_time) {"2014-11-20T09:45:42.025Z"}
+    planner = Hypgen::Planner.new(workflow)
+    expect(planner.days).to eq(1)
+
+  end
+
+    it 'should compute performance model' do
+
+    workflow = double("workflow")
+    allow(workflow).to receive(:deadline) {60}
+
+    # these values are not used in the test
+    allow(workflow).to receive(:start_time) {"2014-11-19T09:45:42.025Z"}
+    allow(workflow).to receive(:end_time) {"2014-11-20T09:45:42.025Z"}
     planner = Hypgen::Planner.new(workflow)
 
     # test some known examples
