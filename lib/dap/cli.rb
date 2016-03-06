c7a1811959d6724170536fa97e34a2fd45e59ba8
Maciej Pawlik <m.pawlik@cyfronet.pl>
2016-03-06 21:06:40 +0100

    Add DAP::create_threat_assessment

--- lib/dap/cli.rb
+++ lib/dap/cli.rb
@@ -5,6 +5,7 @@ module Dap
   class Cli
     include RestCli
 
+    #TODO: remove unused sections_ids
     def create_threat_assessment_run(name, section_ids, start_time, end_time)
       puts "1. creating new threat assessment run for #{section_ids} profiles with period #{start_time} - #{end_time}"
 
@@ -37,9 +38,31 @@ module Dap
 
       raise(ThreatAssessmentRunUpdateError, response.body) unless response.status == 200
     end
+
+    def create_threat_assessment(threat_assessment_run_id, profiles)
+      puts "1a. creating new threat assesment for #{profiles} profiles"
+
+      response = connection.post do |req|
+        req.url '/api/v1/threat_assessments'
+        req.headers['Content-Type'] = 'application/json'
+        req.body = {
+            threat_assessment: {
+                profiles: profiles,
+                threat_assessment_run_id: threat_assessment_run_id
+            }
+        }.to_json
+      end
+
+      raise(ThreatAssessmentCreationError, response.body) unless response.status == 201
+
+      JSON.parse(response.body)['threat_assessment']['id']
+    end
   end
 
   class ThreatAssessmentRunCreationError < Exception
+  end
+
+  class ThreatAssessmentCreationError < Exception
   end
 
   class ThreatAssessmentRunUpdateError < Exception
